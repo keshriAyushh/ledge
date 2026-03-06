857059d6e061644ff7df788a0264c5cc14a0aea6
Ayush <keshri.ayush019@gmail.com>
2026-03-06 21:20:35 +0530

    added network module

---
+++ core/network/src/main/java/com/ayush/network/di/NetworkModule.kt
@@ -0,0 +1,38 @@
+package com.ayush.network.di
+
+import com.ayush.network.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideSupabaseClient(): SupabaseClient {
+        return createSupabaseClient(
+            supabaseUrl = BuildConfig.SUPABASE_URL,
+            supabaseKey = BuildConfig.SUPABASE_ANON_KEY
+        ) {
+            install(Auth)
+            install(Postgrest)
+            install(Storage)
+        }
+    }
+
+    @Provides
+    @Named("google_web_client_id")
+    fun provideGoogleWebClientId(): String {
+        return BuildConfig.GOOGLE_WEB_CLIENT_ID
+    }
+}
